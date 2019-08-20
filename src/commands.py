e857387cb0e22fcc0bff83e1c2d9e61494bf30fb
Cédric <nope-mcnopeface@asdf.org>
2019-08-20 19:48:07 -0400

    Added /info [Container]

--- src/commands.py
+++ src/commands.py
@@ -1,4 +1,6 @@
+import docker
 from docker import DockerClient
+import logging
 from telegram import Bot, KeyboardButton, ParseMode, ReplyKeyboardMarkup
 from telegram.ext import Updater
 from typing import Callable, Dict, List, Optional, Union
@@ -11,6 +13,7 @@ Command = Callable[[DockerClient, Bot, Updater, List[str]], None]
 
 
 commands = {}  # type: Dict[str, Command]
+commands_help = {}  # type: Dict[str, str]
 command_keyboard = [
     [
         "/start", "/info", "/help"
@@ -18,7 +21,7 @@ command_keyboard = [
 ]  # type: List[List[Union[str, KeyboardButton]]]
 
 
-def _command(name: Optional[str] = None):
+def _command(name: Optional[str] = None, help_msg: str = ""):
     """Decorator for command callbacks.
 
     A command callback has type commands.Command.
@@ -26,60 +29,81 @@ def _command(name: Optional[str] = None):
     def decorator(cmd: Command):
         if name:
             global commands
+            global commands_help
             commands[name] = cmd
+            commands_help[name] = help_msg
         return cmd
     return decorator
 
 
-@_command("help")
+@_command(
+    "help",
+    """Usage `/help COMMAND`
+Displays help message about `COMMAND` if available.""")
 def command_help(client: DockerClient,
                  bot: Bot,
                  update: Updater,
                  args: List[str]) -> None:
-    """Usage `/help COMMAND`
-    Displays help message about COMMAND if available.
-    """
-    if not assert_arg_count(1, args, bot, update):
+    if not expect_arg_count(1, args, bot, update):
         return
     command_name = args[0]
     if command_name not in commands:
         reply_error(f'Command `{command_name}` not found.', bot, update)
         return
-    command_doc = commands[command_name].__doc__
+    command_doc = commands_help.get(command_name, None)
     if command_doc is None:
-        reply(f'No help available for command `{command_name}` ☹️',
+        reply(f'No help available for command `{command_name}`.',
               bot, update)
     else:
-        reply(f'🆘 **Help for command `{command_name}`** 🆘\n{command_doc}',
+        reply(f'''🆘 *Help for command `{command_name}`* 🆘
+{command_doc}''',
               bot, update)
 
 
-@_command("info")
+@_command(
+    "info",
+    """Usage: `/info [CONTAINER]`
+Provides global informations about the current docker daemon, or about `CONTAINER`, if specified.""")
 def command_info(client: DockerClient,
                  bot: Bot,
                  update: Updater,
                  args: List[str]) -> None:
-    """Usage: `/info`
-    Provides global informations about the current docker daemon.
-    """
-    info = client.info()
-    message = f'''**Docker status** 🐳⚙️
+    if not expect_max_arg_count(1, args, bot, update):
+        return
+    if len(args) == 0:
+        info = client.info()
+        message = f'''*Docker status* 🐳⚙️
 ▪️ Docker version: {info["ServerVersion"]}
 ▪️ Memory: {info["MemTotal"]}
 ▪️ Running containers: {info["ContainersRunning"]}
 ▪️ Paused containers: {info["ContainersPaused"]}
 ▪️ Stopped containers: {info["ContainersStopped"]}'''
-    reply(message, bot, update)
+        reply(message, bot, update)
+    else:
+        container_id = args[0]
+        logging.debug(container_id)
+        try:
+            c = client.containers.get(container_id)
+            message = f'''*Container `{c.short_id} {c.name}`:*
+️️️️️️️️▪️ Image: {c.image}
+️️️️️️️️▪️ Status: {c.status}
+️️️️️️️️▪️ Labels: {c.labels}'''
+            reply(message, bot, update)
+        except docker.errors.NotFound:
+            reply_error(f'Container \"{container_id}\" not found.', bot,
+                        update)
+        except docker.errors.APIError as e:
+            reply_error(f'Docker daemon error: {str(e)}', bot, update)
 
 
-@_command("start")
+@_command(
+    "start",
+    """Usage: `/start`
+(Re)initializes the bot's internal state for that user.""")
 def command_start(client: DockerClient,
                   bot: Bot,
                   update: Updater,
                   args: List[str]) -> None:
-    """Usage: `/start`
-    (Re)initializes the bot's internal state for that user.
-    """
     reply(f'Hello {update.message.from_user.first_name} 👋 I am your personal '
           f'docker assistant. Please select a command.',
           bot,
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ from telegram import Bot, ParseMode, Update
 from typing import List
 
 
-def assert_arg_count(arg_count: int,
+def expect_arg_count(arg_count: int,
                      arg_list: List[str],
                      bot: Bot,
                      update: Update) -> bool:
@@ -14,10 +14,32 @@ def assert_arg_count(arg_count: int,
     return True
 
 
+def expect_max_arg_count(max_arg_count: int,
+                         arg_list: List[str],
+                         bot: Bot,
+                         update: Update) -> bool:
+    if len(arg_list) > max_arg_count:
+        reply_error(f'This function expects at most {max_arg_count} '
+                    f'argument(s).', bot, update)
+        return False
+    return True
+
+
+def expect_min_arg_count(min_arg_count: int,
+                         arg_list: List[str],
+                         bot: Bot,
+                         update: Update) -> bool:
+    if len(arg_list) < min_arg_count:
+        reply_error(f'This function expects at least {min_arg_count} '
+                    f'argument(s).', bot, update)
+        return False
+    return True
+
+
 def reply(message: str, bot: Bot, update: Update, **kwargs) -> None:
     bot.send_message(
         chat_id=update.message.chat_id,
-        parse_mode=ParseMode.MARKDOWN,
+        parse_mode='Markdown',
         reply_to_message_id=update.message.message_id,
         text=message,
         **kwargs
@@ -25,12 +47,12 @@ def reply(message: str, bot: Bot, update: Update, **kwargs) -> None:
 
 
 def reply_error(message: str, bot: Bot, update: Update) -> None:
-    reply(f'❌ **ERROR** ❌\n{message}', bot, update)
+    reply(f'❌ *ERROR* ❌\n{message}', bot, update)
     logging.error(f'User "{update.message.from_user.username}" raised an '
                   f'error: {message}')
 
 
 def reply_warning(message: str, bot: Bot, update: Update) -> None:
-    reply(f'⚠️ **WARNING** ⚠️\n{message}', bot, update)
+    reply(f'⚠️ *WARNING* ⚠️\n{message}', bot, update)
     logging.warning(f'User "{update.message.from_user.username}" raised a '
                     f'warning: {message}')
